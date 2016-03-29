eecbe55e74ec3f8a57432a050f62577845a6c2ba
Josh Tatterton <joshua.tatterton@matalandirect.com>
2016-03-29 15:01:30 +0100

    Add print contacts + add new contact to loop ctrl

--- address_book.rb
+++ address_book.rb
@@ -11,14 +11,31 @@ class AddressBook
 	def run
 		loop do
 			puts "Address Book"
+			puts "a: Add Contact"
+			puts "p: Print Address Book"
 			puts "e: Exit"
 			print "Enter your choice: "
 			input = gets.chomp.downcase
 			case input
+			when "a"
+				add_contact
+			when "p"
+				print_contact_list
 			when "e"
 				break
 			end
 		end
+	end
+
+	def add_contact
+		contact = Contact.new
+		print "First name: "
+		contact.first_name = gets.chomp
+		print "Middle name: "
+		contact.middle_name = gets.chomp
+		print "Last name: "
+		contact.last_name = gets.chomp
+		contacts.push(contact)
 	end
 
 	def print_results(search, results)
